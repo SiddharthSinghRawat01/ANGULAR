75bb544e3d3493786e5d477b47aa18b2227a3acd
Siddharth Singh Rawat <siddharthsinghrawat01@gmail.com>
2024-04-02 16:10:04 +0530

    sending data form parent to child @Input property

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,67 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  type = 2;
-  count = [];
-  products = [1,2,3,4,5]
+  products = [{
+    "id": "1",
+    "brand": "Heref&Now",
+    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/g/s/q/9-ultraboost-phylon-white-green-9-killer-ultraboost-white-green-original-imagt4sze4uhyenb.jpeg?q=70",
+    currentPrice: "150",
+    standerdPrice: "300",
+    discount: "50",
+    name: "T-shirt"
+  },
+  {
+    "id": "2",
+    "brand": "Heref&Now",
+    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    currentPrice: "150",
+    standerdPrice: "300",
+    discount: "50",
+    name: "T-shirt"
+  },
+  {
+    "id": "3",
+    "brand": "Heref&Now",
+    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    currentPrice: "150",
+    standerdPrice: "300",
+    discount: "50",
+    name: "T-shirt"
+  },
+  {
+    "id": "4",
+    "brand": "Heref&Now",
+    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    currentPrice: "150",
+    standerdPrice: "300",
+    discount: "50",
+    name: "T-shirt"
+  },
+  {
+    "id": "5",
+    "brand": "Heref&Now",
+    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    currentPrice: "150",
+    standerdPrice: "300",
+    discount: "50",
+    name: "T-shirt"
+  },
+  {
+    "id": "6",
+    "brand": "Heref&Now",
+    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    currentPrice: "150",
+    standerdPrice: "300",
+    discount: "50",
+    name: "T-shirt"
+  }]
 }
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
   export class ProductCardComponent {
-    products = [1,2,3,4,5]
+ @Input() product: any;
   }
