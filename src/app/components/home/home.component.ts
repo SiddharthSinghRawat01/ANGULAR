d88080dfd86eaa46b74c169ec47db1207de905e8
Siddharth Singh Rawat <siddharthsinghrawat01@gmail.com>
2024-04-02 13:28:43 +0530

    material card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   type = 2;
   count = [];
+  products = [1,2,3,4,5]
 }
