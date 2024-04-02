a91ff83ed795c297194cd26111bdbedf17cbfc9d
Siddharth Singh Rawat <siddharthsinghrawat01@gmail.com>
2024-04-02 17:22:26 +0530

    two way communication

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent {
   products = [{
     "id": "1",
     "brand": "Heref&Now",
-    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/g/s/q/9-ultraboost-phylon-white-green-9-killer-ultraboost-white-green-original-imagt4sze4uhyenb.jpeg?q=70",
+    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/g/s/q/9-ultraboost-phylon-white-green-9-killer-ultraboost-white-green-original-imagt4sze4uhyenb.jpeg?q=70",
     currentPrice: "150",
     standerdPrice: "300",
     discount: "50",
@@ -22,7 +22,7 @@ export class HomeComponent {
   {
     "id": "2",
     "brand": "Heref&Now",
-    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
     currentPrice: "150",
     standerdPrice: "300",
     discount: "50",
@@ -31,7 +31,7 @@ export class HomeComponent {
   {
     "id": "3",
     "brand": "Heref&Now",
-    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    image: "https://www.merchantmarine.co.in/cdn/shop/products/5014PCSS508M_2_600x.jpg?v=1662485390",
     currentPrice: "150",
     standerdPrice: "300",
     discount: "50",
@@ -40,7 +40,7 @@ export class HomeComponent {
   {
     "id": "4",
     "brand": "Heref&Now",
-    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    image: "https://www.merchantmarine.co.in/cdn/shop/products/5018PCSS524M_2_600x.jpg?v=1662485513",
     currentPrice: "150",
     standerdPrice: "300",
     discount: "50",
@@ -49,7 +49,7 @@ export class HomeComponent {
   {
     "id": "5",
     "brand": "Heref&Now",
-    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
     currentPrice: "150",
     standerdPrice: "300",
     discount: "50",
@@ -58,10 +58,16 @@ export class HomeComponent {
   {
     "id": "6",
     "brand": "Heref&Now",
-    imgae: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
+    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/3/j/v/xxl-st10-vebnor-original-imagnvrqgv7e5crg.jpeg?q=70",
     currentPrice: "150",
     standerdPrice: "300",
     discount: "50",
     name: "T-shirt"
   }]
+
+  onViewProduct(event:any){
+    console.log("onViewProduct",event);
+    
+  }
+
 }
