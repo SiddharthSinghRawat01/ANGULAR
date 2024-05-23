3097cd3dc7920d77a98721da4d7b9755c7df3803
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2024-05-23 12:37:46 +0530

    custome pipe

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -3,11 +3,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../types/product';
+import { RupeePipe } from '../../rupee.pipe';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule, CommonModule, MatButtonModule,UpperCasePipe],
+  imports: [MatCardModule, CommonModule, MatButtonModule,UpperCasePipe,RupeePipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
